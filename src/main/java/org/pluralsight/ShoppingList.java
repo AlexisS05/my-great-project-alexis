f4cfe16e82db6c88cfbaaefca40e31be0002ae98
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-10-05 15:39:34 -0400

    completed ShoppingList App

---
+++ src/main/java/org/pluralsight/ShoppingList.java
@@ -0,0 +1,5 @@
+package org.pluralsight;
+
+public class ShoppingList {
+    String[] items = {"Apples", "Oranges", "Orange Juice", "Milk", "Eggs", "Toast", "Tortillas", "Cereals", "Yogurt", "Ketchup"};
+}
